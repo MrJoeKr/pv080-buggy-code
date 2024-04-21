0e2b47d158f503f91188abe9acac02d08f0080bf
MrJoeKr <kraus.jozef225@gmail.com>
2024-04-21 22:31:45 +0200

    Fix other stuff

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,10 +1,12 @@
 import flask
+import urllib2
+import urllib3
 import yaml
 
-app = flask.Flask(__name__)
+APP = flask.Flask(__name__)
 
 
-@app.route("/")
+@APP.route("/")
 def index():
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
@@ -26,12 +28,13 @@ def print_nametag(format_string, person):
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     if urllib_version == 2:
-        import urllib2 as urllib
+        http = urllib2.PoolManager()
     elif urllib_version == 3:
-        import urllib3 as urllib
+        http = urllib3.PoolManager()
+    else:
+        raise ValueError("Invalid urllib version!")
 
     # Fetch and print the requested URL
-    http = urllib.PoolManager()
     r = http.request("GET", url)
 
     print(r.data)
@@ -59,16 +62,16 @@ if __name__ == "__main__":
     print("2. Code injection vulnerability:")
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
-    choice = input("Select vulnerability: ")
-    if choice == "1":
+    CHOICE = input("Select vulnerability: ")
+    if CHOICE == "1":
         new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
-    elif choice == "2":
-        urlib_version = input("Choose version of urllib: ")
-        fetch_website(urlib_version, url="https://www.google.com")
-    elif choice == "3":
+    elif CHOICE == "2":
+        URLIB_VERSION = input("Choose version of urllib: ")
+        fetch_website(URLIB_VERSION, url="https://www.google.com")
+    elif CHOICE == "3":
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
-    elif choice == "4":
-        password = input("Enter master password: ")
-        authenticate(password)
+    elif CHOICE == "4":
+        PASSWORD = input("Enter master password: ")
+        authenticate(PASSWORD)
